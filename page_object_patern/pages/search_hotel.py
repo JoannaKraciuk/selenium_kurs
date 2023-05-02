6784b824364eedcc2e3a4beb3e5369926b9625cc
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-05-02 22:07:11 +0200

    commit search_hotel

--- page_object_patern/pages/search_hotel.py
+++ page_object_patern/pages/search_hotel.py
@@ -7,10 +7,10 @@ class SearchHotelPage:
         self.location_match_span_xpath = "//span[text()='Dubai']"
         self.check_in_input_name = "checkin"
         self.check_out_input_name = "checkout"
-        self.travelers_input_id = "travelersInput"
+        self.travellers_input_id = "travellersInput"
         self.adult_input_id = "adultInput"
         self.child_input_id = "childInput"
-        self.search_button_xpath = "//button[text()='Search']"
+        self.search_button_xpath = "//button[text()=' Search']"
 
     def set_city(self, city):
         self.driver.find_element_by_xpath(self.search_hotel_span_xpath).click()
@@ -20,17 +20,17 @@ class SearchHotelPage:
     def set_date_range(self, check_in, check_out):
 
         self.driver.find_element_by_name(self.check_in_input_name).send_keys(check_in)
-        self.driver.find_element_by_name(self.checkout_input_name).send_keys(check_out)
+        self.driver.find_element_by_name(self.check_out_input_name).send_keys(check_out)
 
     def set_travellers(self, adults, child):
 
-        self.driver.find_element_by_id(self.travelers_input_id).click()
-        self.driver.find_element_by_id(self.adult_input_id).clean()
+        self.driver.find_element_by_id(self.travellers_input_id).click()
+        self.driver.find_element_by_id(self.adult_input_id).clear()
         self.driver.find_element_by_id(self.adult_input_id).send_keys(adults)
-        self.driver.find_element_by_id(self.child_input_id).clean()
+        self.driver.find_element_by_id(self.child_input_id).clear()
         self.driver.find_element_by_id(self.child_input_id).send_keys(child)
 
-    def perfom_search(self):
+    def perform_search(self):
         self.driver.find_element_by_xpath(self.search_button_xpath).click()
 
 
