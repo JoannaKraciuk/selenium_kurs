210c7b894960fb22fbcc80d37edd1c1eed89860a
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-05-02 22:06:55 +0200

    commit test_hotel_search

--- page_object_patern/tests/test_hotel_search.py
+++ page_object_patern/tests/test_hotel_search.py
@@ -4,7 +4,7 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from page_object_patern.pages.search_hotel import SearchHotelPage
-
+from page_object_patern.pages.search_results import SearchResultPage
 
 class TestHotelSearch:
 
@@ -19,7 +19,20 @@ class TestHotelSearch:
     def test_hotel_search(self, setup):
         self.driver.get("http://www.kurs-selenium.pl/demo/")
         search_hotel_page = SearchHotelPage(self.driver)
+        search_hotel_page.set_city("Dubai")
         search_hotel_page.set_date_range("01/05/2023", "06/05/2023")
         search_hotel_page.set_travellers("2", "2")
-        search_hotel_page.perfom_search()
+        search_hotel_page.perform_search()
+        results_page = SearchResultPage(self.driver)
+        hotel_names = results_page.get_hotel_names()
+        price_values = results_page.get_hotel_prices()
 
+        assert hotel_names[0] == "Jumeirah Beach Hotel"
+        assert hotel_names[1] == "Oasis Beach Tower"
+        assert hotel_names[2] == "Rose Rayhaan Rotana"
+        assert hotel_names[3] == "Hyatt Regency Perth"
+
+        assert price_values[0] == "$22"
+        assert price_values[1] == "$50"
+        assert price_values[2] == "$80"
+        assert price_values[3] == "$150"
