093cccae9523474b85a2d14554dba98fb99c5fb1
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-05-06 12:36:32 +0200

    commit search_results1

--- page_object_patern/pages/search_results.py
+++ page_object_patern/pages/search_results.py
@@ -1,23 +1,16 @@
+from page_object_patern.locators.locators import SearchResultLocators
 import logging
 class SearchResultsPage:
     def __init__(self, driver):
         self.driver = driver
         self.logger = logging.getLogger(__name__)
-        self.hotel_names_xpath = "//h4[contains(@class,'list_title')]//b"
-        self.hotel_prices_xpath = "//div[contains(@class,'price_tab')]//b"
+        self.hotel_names_xpath = SearchResultLocators.hotel_names_xpath
+        self.hotel_prices_xpath = SearchResultLocators.hotel_prices_xpath
 
     def get_hotel_names(self):
-        hotels = self.driver.find_element_by_xpath(self.hotel_names_xpath)
-        names = [hotel.get_attribute("textContent") for hotel in hotels]
-        self.logger.info("Avaliable hotels are: ")
-        for name in names:
-            self.logger.info(name)
-        return names
+        hotels = self.driver.find_elements_by_xpath(self.hotel_names_xpath)
+        return [hotel.get_attribute("textContent") for hotel in hotels]
 
     def get_hotel_prices(self):
-        prices = self.driver.find_element_by_xpath(self.hotel_prices_xpath)
-        hotel_prices = [price.get_attribute("textContent") for price in prices]
-        self.logger.info("Prices are: ")
-        for price in prices:
-            self.logger.info(price)
-        return prices
+        prices = self.driver.find_elements_by_xpath(self.hotel_prices_xpath)
+        return [price.get_attribute("textContent") for price in prices]
