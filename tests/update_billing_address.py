60c26b91dd4572948ff3ffd60d10828ffc52e8d5
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-05-09 13:54:43 +0200

    commit update billing address

---
+++ tests/update_billing_address.py
@@ -0,0 +1,26 @@
+import random
+
+import pytest
+
+from pages.billing_address_page import BillingAddressPage
+from pages.my_account_page import MyAccountPage
+
+
+@pytest.mark.usefixtures("setup")
+class TestUpdateBillingAddress:
+
+    def test_update_billing_address(self):
+        email = str(random.randint(0, 10000)) + "sialalala@gmail.com"
+        my_account_page = MyAccountPage(self.driver)
+        my_account_page.open_page()
+        my_account_page.create_account(email, "Tester12345!@#$%")
+        billing_address_page = BillingAddressPage(self.driver)
+        billing_address_page.open_edit_billing_address()
+        billing_address_page.set_personal_data("John", "Doe")
+        billing_address_page.set_company_name("NewOne")
+        billing_address_page.select_country("Poland")
+        billing_address_page.set_address("Olchowa", "00-140", "Krakow")
+        billing_address_page.set_phone_number("123456678")
+        billing_address_page.save_address()
+
+        assert "Address changed successfully." in billing_address_page.get_message_text()
