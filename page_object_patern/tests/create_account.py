8b4ff22838e43739d700c50f776e9cf245a8357b
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-05-11 09:35:59 +0200

    commit create account

--- page_object_patern/tests/create_account.py
+++ page_object_patern/tests/create_account.py
@@ -1,8 +1,6 @@
 import random
-
 import pytest
-
-from pages.my_account_page import MyAccountPage
+from page_object_patern.pages.my_account_page import MyAccountPage
 
 
 @pytest.mark.usefixtures("setup")
@@ -14,7 +12,7 @@ class TestCreateAccount:
         my_account_page.create_account("sialalala@gmail.com", "Tester12345!@#$%")
 
         msg = "Error: An account is already registered with your email address. Please log in."
-        assert msg in my_account_page.get_error_msg
+        assert msg in my_account_page.get_error_msg()
 
     def test_create_account_passed(self):
         email = str(random.randint(0, 10000)) + "sialalala@gmail.com"
