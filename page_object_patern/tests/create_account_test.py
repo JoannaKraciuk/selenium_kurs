77b50856b65c012f37c6115c20aba9b03aeac035
Joanna Kraciuk <103620609+JoannaKraciuk@users.noreply.github.com>
2023-05-12 15:24:14 +0200

    commit add description test

--- page_object_patern/tests/create_account_test.py
+++ page_object_patern/tests/create_account_test.py
@@ -1,4 +1,5 @@
 import random
+import allure
 import pytest
 from page_object_patern.pages.my_account_page import MyAccountPage
 
@@ -6,6 +7,8 @@ from page_object_patern.pages.my_account_page import MyAccountPage
 @pytest.mark.usefixtures("setup")
 class TestCreateAccount:
 
+    @allure.title("Create account test")
+    @allure.description("This test execute creating account with invalid data")
     def test_create_account_failed(self):
         my_account_page = MyAccountPage(self.driver)
         my_account_page.open_page()
@@ -14,6 +17,7 @@ class TestCreateAccount:
         msg = "Error: An account is already registered with your email address. Please log in."
         assert msg in my_account_page.get_error_msg()
 
+    @allure.description("This test execute creating account with valid data")
     def test_create_account_passed(self):
         email = str(random.randint(0, 10000)) + "sialalala@gmail.com"
         my_account_page = MyAccountPage(self.driver)
